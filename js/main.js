973eadd5da31528f9bd97f6aadc2e9c156a9c0cb
abdi-El <abdila1099@gmail.com>
2021-11-15 15:01:35 +0100

    done

--- js/main.js
+++ js/main.js
@@ -3,18 +3,36 @@ const app = new Vue({
     data: {
         todos:[
             {
-                text: 'fare la spesa',
+                text: 'Fare la spesa',
                 completed: false
             },
             {
-                text: 'fare la todolist con VueJS',
+                text: 'Fare la todolist con VueJS',
                 completed: false
             },
             {
-                text: 'fare esercizo',
+                text: 'Fare esercizo',
                 completed: true
             }
-        ]
+        ],
+        newTodo: '',
+    },
+    methods:{
+        add(){
+            if (this.newTodo.length > 0){
+                let newTodo = {};
+                newTodo.text = this.newTodo;
+                newTodo.completed = false;
+                this.todos.unshift(newTodo);
+                this.newTodo = '';
+            } 
+        },
+        remove(listIndex){
+            this.todos.splice(listIndex, 1);
+        },
+        completed(listIndex){
+           this.todos[listIndex].completed = !this.todos[listIndex].completed ;
+        }
     }
 
 })
