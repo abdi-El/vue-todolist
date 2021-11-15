e722151c6bb13cb9416016dbf30e1ffb78944334
abdi-El <abdila1099@gmail.com>
2021-11-15 13:18:31 +0100

    add VueJs

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        todos:[
+            {
+                text: 'fare la spesa',
+                completed: false
+            },
+            {
+                text: 'fare la todolist con VueJS',
+                completed: false
+            },
+            {
+                text: 'fare esercizo',
+                completed: true
+            }
+        ]
+    }
+
+})
